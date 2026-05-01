9b49218717dd6e93e861d97a120880e5eced9b9e
Tabitha Mburu <mburutabitha373@gmail.com>
2026-05-01 12:53:53 +0300

    Finalize shopping app features for grading

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
 function ProductCard({ product, addToCart }) {
   return (
-    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+    <article style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
       <h3>{product.name}</h3>
       <p>{product.category}</p>
 
       <button onClick={() => addToCart(product)}>
         Add to Cart
       </button>
-    </div>
+    </article>
   );
 }
 
